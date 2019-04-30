f6efbc674a19603b6e7cbf82a4df4b96a4acb519
Humaniuc <humancoria@gmail.com>
2019-04-30 19:32:51 +0300

    Read number of array elements

--- WantsomeHomework/FindDuplicatesInArray/Program.cs
+++ WantsomeHomework/FindDuplicatesInArray/Program.cs
@@ -10,6 +10,26 @@ namespace FindDuplicatesInArray
     {
         static void Main(string[] args)
         {
+            uint nuElem = ReadPositiveInteger("Write a positive integer number: ");
+        }
+
+        private static uint ReadPositiveInteger(string text)
+        {
+            uint number;
+            bool correct;
+
+            Console.Write(text);
+            do
+            {
+                correct = uint.TryParse(Console.ReadLine(), out number);
+                if (!correct)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.Write(text);
+                }
+            } while (!correct);
+
+            return number;
         }
     }
 }
