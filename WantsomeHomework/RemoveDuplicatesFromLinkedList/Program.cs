0d7b034e03f49f7f98c2afc2fcb9fc9557cb95fd
Humaniuc <humancoria@gmail.com>
2019-05-01 19:51:01 +0300

    Creating and printing a Linked List

--- WantsomeHomework/RemoveDuplicatesFromLinkedList/Program.cs
+++ WantsomeHomework/RemoveDuplicatesFromLinkedList/Program.cs
@@ -10,6 +10,48 @@ namespace RemoveDuplicatesFromLinkedList
     {
         static void Main(string[] args)
         {
+            LinkedList<int> list = CreateList();
+            
+            PrintList(list);
+        }
+
+        private static int ReadInteger(string text, int lowerLimit, int upperLimit)
+        {
+            int number;
+            bool correct;
+            Console.Write(text);
+            do
+            {
+                correct = int.TryParse(Console.ReadLine(), out number);
+                if (!correct || number < lowerLimit || number > upperLimit)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.Write(text);
+                }
+            } while (!correct || number < lowerLimit || number > upperLimit);
+            return number;
+        }
+
+        private static LinkedList<int> CreateList()
+        {
+            Console.WriteLine("Press enter to continue reading!");
+            LinkedList<int> list = new LinkedList<int>();
+            do
+            {
+                LinkedListNode<int> node = new LinkedListNode<int>(ReadInteger("Write an list element: ", int.MinValue, int.MaxValue));
+                list.AddLast(node);
+            } while (Console.ReadKey().Key == ConsoleKey.Enter);
+            Console.WriteLine();
+            return list;
+        }
+
+        private static void PrintList(LinkedList<int> list)
+        {
+            foreach(int node in list)
+            {
+                Console.Write($"{node} ");
+            }
+            Console.WriteLine();
         }
     }
 }
