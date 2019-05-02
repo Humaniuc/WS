9b2a3b1843cff53280a6cb2682183cac0eb0f4aa
Humaniuc <humancoria@gmail.com>
2019-05-03 00:03:41 +0300

    Verified Palindrome

--- WantsomeHomework/PalindromeNumber/Program.cs
+++ WantsomeHomework/PalindromeNumber/Program.cs
@@ -11,7 +11,14 @@ namespace PalindromeNumber
         static void Main(string[] args)
         {
             int number = ReadNumber("Write an integer: ");
-
+            if(Palindrome(number))
+            {
+                Console.WriteLine($"{number} is palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not palindrome");
+            }
 
         }
 
@@ -30,6 +37,19 @@ namespace PalindromeNumber
                 }
             } while (!correct);
             return number;
+        }
+
+        private static bool Palindrome(int number)
+        {
+            string num = number.ToString();
+            for(int i = 0, j = num.Length-1; i < j; i++, j--)
+            {
+                if(num[i] != num[j])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
