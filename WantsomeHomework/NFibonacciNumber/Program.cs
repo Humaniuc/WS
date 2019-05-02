2057441d7f559ff83111cd7461feb4d44bf33167
Humaniuc <humancoria@gmail.com>
2019-05-02 06:41:40 +0300

    Calculate nth Fibonacci number

--- WantsomeHomework/NFibonacciNumber/Program.cs
+++ WantsomeHomework/NFibonacciNumber/Program.cs
@@ -8,8 +8,34 @@ namespace NFibonacciNumber
 {
     class Program
     {
+        private const double GoldenRatio = 1.61803398874989484820;
+
         static void Main(string[] args)
         {
+            uint n = ReadPositiveInteger("Write a positive integer: ");
+            Console.WriteLine($"The {n}{(n==1 ? "st" : (n==2)? "nd" : "th")} Fibonacci number is: {NFibonacci(GoldenRatio, n)}");
+        }
+
+        private static uint ReadPositiveInteger(string text)
+        {
+            uint n;
+            bool correct;
+            Console.Write(text);
+            do
+            {
+                correct = uint.TryParse(Console.ReadLine(), out n);
+                if (!correct)
+                {
+                    Console.WriteLine("invalid Input");
+                    Console.Write(text);
+                }
+            } while (!correct);
+            return n;
+        }
+        private static long NFibonacci(double goldenRatio, uint n)
+        {
+            long fibNum = (long)((Math.Pow(goldenRatio, n) - Math.Pow((1 - goldenRatio), n)) / Math.Sqrt(5));
+            return fibNum;
         }
     }
 }
