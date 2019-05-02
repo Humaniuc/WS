c172cf3400345e1cfcd9e22f4d7e7cf8de739d03
Humaniuc <humancoria@gmail.com>
2019-05-03 01:28:51 +0300

    fix

--- WantsomeHomework/18.ReverseLinkedList/Program.cs
+++ WantsomeHomework/18.ReverseLinkedList/Program.cs
@@ -10,8 +10,54 @@ namespace E18.ReverseLinkedList
     {
         static void Main(string[] args)
         {
+            LinkedList<int> list = CreateList();
+            PrintList(list);
         }
 
+        private static int ReadInteger(string text, int lowerLimit, int upperLimit)
+        {
+            int number;
+            bool correct;
+            Console.Write(text);
+            do
+            {
+                correct = int.TryParse(Console.ReadLine(), out number);
+                if (!correct || number < lowerLimit || number > upperLimit)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.Write(text);
+                }
+            } while (!correct || number < lowerLimit || number > upperLimit);
+            return number;
+        }
 
+        private static int[] CreateArray(int number)
+        {
+            int[] myArr = new int[number];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                myArr[i] = ReadInteger($"listNode{i}= ", int.MinValue, int.MaxValue);
+            }
+            return myArr;
+        }
+
+        private static LinkedList<int> CreateList()
+        {
+            int number = ReadInteger("Enter number of list elements: ", 0, int.MaxValue);
+
+            int[] myArr = CreateArray(number);
+            LinkedList<int> list = new LinkedList<int>(myArr);
+
+            return list;
+        }
+
+        private static void PrintList(LinkedList<int> list)
+        {
+            foreach (int node in list)
+            {
+                Console.Write($"{node} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
