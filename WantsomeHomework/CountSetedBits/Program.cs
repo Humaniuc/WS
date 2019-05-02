2935e41ebe9a70b65803bed9f361c3b845418ef6
Humaniuc <humancoria@gmail.com>
2019-05-02 06:58:46 +0300

    Create function for count number of seted bits.

--- WantsomeHomework/CountSetedBits/Program.cs
+++ WantsomeHomework/CountSetedBits/Program.cs
@@ -10,6 +10,37 @@ namespace CountSetedBits
     {
         static void Main(string[] args)
         {
+            int number = ReadInteger("Write an integer number: ");
+            Console.WriteLine($"Number {number} has {SetedBitsCount(number)} seted bits");
+        }
+
+        private static int ReadInteger(string text)
+        {
+            int number;
+            bool correct;
+            Console.Write(text);
+            do
+            {
+                correct = int.TryParse(Console.ReadLine(), out number);
+                if (!correct)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.Write(text);
+                }
+            } while (!correct);
+            return number;
+        }
+        private static int SetedBitsCount(int number)
+        {
+            int count = 0;
+            for(int i = 0; i < 32; i++)
+            {
+                if((number & (1 << i)) != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
