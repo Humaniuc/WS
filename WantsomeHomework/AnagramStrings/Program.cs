eca048495b829e8bce8ba4b1cabb6a6dbed20106
Humaniuc <humancoria@gmail.com>
2019-05-02 06:08:03 +0300

    Create two equal length strings

--- WantsomeHomework/AnagramStrings/Program.cs
+++ WantsomeHomework/AnagramStrings/Program.cs
@@ -10,6 +10,26 @@ namespace AnagramStrings
     {
         static void Main(string[] args)
         {
+            string[] strings = CreateStrings();
+        }
+
+        private static string[] CreateStrings()
+        {
+            string[] strings = new string[2];
+            Console.Write("Write firstString: ");
+            strings[0] = Console.ReadLine();
+            Console.Write("Write second string (same length like firstString): ");
+            do
+            {
+                strings[1] = Console.ReadLine();
+                if (strings[1].Length != strings[0].Length)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("Write second string (same length like firstString): ");
+                }
+            } while (strings[1].Length != strings[0].Length);
+
+            return strings;
         }
     }
 }
