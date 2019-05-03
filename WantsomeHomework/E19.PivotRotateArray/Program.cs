63baa20385b2b8a73bb97459ab2c40ace2198f24
Humaniuc <humancoria@gmail.com>
2019-05-03 09:58:07 +0300

    CreateArray

--- WantsomeHomework/E19.PivotRotateArray/Program.cs
+++ WantsomeHomework/E19.PivotRotateArray/Program.cs
@@ -10,7 +10,46 @@ namespace E19.PivotRotateArray
     {
         static void Main(string[] args)
         {
+            int number = ReadInteger("Write number of array elements: ", 0, int.MaxValue);
+            int[] arr = CreateArray(number);
+            PrintArray(arr);
+        }
 
+        private static int ReadInteger(string text, int lowerLimit, int upperLimit)
+        {
+            int elements;
+            bool correct;
+            Console.Write(text);
+            do
+            {
+                correct = int.TryParse(Console.ReadLine(), out elements);
+                if (!correct || elements < lowerLimit || elements > upperLimit)
+                {
+                    Console.WriteLine("Invalid input");
+                    Console.Write(text);
+                }
+            } while (!correct || elements < lowerLimit || elements > upperLimit);
+            return elements;
+        }
+
+        private static int[] CreateArray(int elements)
+        {
+            int[] myArr = new int[elements];
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                myArr[i] = ReadInteger($"myArr[{i}]= ", int.MinValue, int.MaxValue);
+            }
+
+            return myArr;
+        }
+
+        private static void PrintArray(int[] myArr)
+        {
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                Console.Write($"{myArr[i]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
